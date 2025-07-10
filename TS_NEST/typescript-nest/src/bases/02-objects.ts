c44f0786b60104b95bb225c4ac90027b1c9df6c6
Maury SO <lynuxazura@gmail.com>
2025-07-10 15:21:55 -0600

    Class 2

--- TS_NEST/typescript-nest/src/bases/02-objects.ts
+++ TS_NEST/typescript-nest/src/bases/02-objects.ts
@@ -1,5 +1,40 @@
 
 // Arreglos de numeros
-export const pokemonIds = [1, 2, 3, 4]
+// TypeScript inferiere los tipos en base a su contenido definido
+// de manera implicita
+// export const pokemonIds = [1, 2, 3, 4]
 
-pokemonIds.push("1")
+
+// Error al intentar agregar un string
+// pokemonIds.push(+"2") //Conversion de string a numero
+
+
+
+// Definicion de una interface para el pokemon
+interface Pokemon {
+  id: number,
+  name: string,
+  age?: number // Permite definir un campo como opcional
+  // edad: number | undefined  --> Define que tiene que tener algun valor pero
+  // tiene que venir en el objeto
+}
+
+// Objeto basado en la interface
+export const pikachu: Pokemon = {
+  id: 1,
+  name: "Pikachu",
+}
+
+export const charmander: Pokemon = {
+  id: 2,
+  name: "Charmander",
+  age: 20
+}
+
+// Objetos en array
+
+export const pokemons = [];
+
+pokemons.push(1, 'string', charmander)
+
+
--- TS_NEST/typescript-nest/src/main.ts
+++ TS_NEST/typescript-nest/src/main.ts
@@ -1,8 +1,10 @@
 // Destructuracion de un modulo
 // import { name, edad, TemplateString } from "./bases/01-types"
 
+import { pokemons } from "./bases/02-objects"
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-  </div>
-`
+    ${pokemons.join(',')}
+  </div>`
 
